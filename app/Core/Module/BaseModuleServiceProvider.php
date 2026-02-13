245687795f8ad0e7767a74e3eaaa81550b1efc3c
vite-spirite <boulangermatheo62@gmail.com>
2026-02-13 23:00:39 +0100

    add load commands in module directory

--- app/Core/Module/BaseModuleServiceProvider.php
+++ app/Core/Module/BaseModuleServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Core\Module;
 
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
+use ReflectionClass;
 
 abstract class BaseModuleServiceProvider extends ServiceProvider
 {
@@ -17,11 +18,12 @@ abstract class BaseModuleServiceProvider extends ServiceProvider
     {
         $this->registerRoutes();
         $this->registerMigrations();
+        $this->registerCommands();
     }
 
     protected function registerRoutes(): void
     {
-        $path = $this->getModulePath().'/../Routes/web.php';
+        $path = $this->getModulePath() . '/../Routes/web.php';
 
         if (File::exists($path)) {
             $this->loadRoutesFrom($path);
@@ -37,9 +39,42 @@ abstract class BaseModuleServiceProvider extends ServiceProvider
 
     protected function registerMigrations(): void
     {
-        $path = $this->getModulePath().'/../Migrations';
+        $path = $this->getModulePath() . '/../Migrations';
         if (File::isDirectory($path)) {
             $this->loadMigrationsFrom($path);
         }
+    }
+
+    protected function registerCommands(): void
+    {
+        if (!$this->app->runningInConsole()) {
+            return;
+        }
+
+        $commandPath = $this->getModulePath() . '/../Commands';
+
+        if (!File::isDirectory($commandPath)) {
+            return;
+        }
+
+        $commands = [];
+        $files = File::allFiles($commandPath);
+
+        foreach ($files as $file) {
+            $relativePath = str_replace($commandPath, '', $file->getPathname());
+            $relativePath = str_replace(['/', '.php'], ['\\', ''], $relativePath);
+
+            $reflection = new ReflectionClass($this);
+            $namespace = $reflection->getNamespaceName();
+            $moduleNamespace = preg_replace('/\\\\Providers$/', '', $namespace);
+
+            $commandClass = $moduleNamespace . '\Commands' . $relativePath;
+
+            if (class_exists($commandClass)) {
+                $commands[] = $commandClass;
+            }
+        }
+
+        $this->commands($commands);
     }
 }
